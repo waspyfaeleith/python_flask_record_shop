73eba782526801ad95e1a1c7c4f5e0cefe5a18ed
Sandy McMillan <sandy.mcmillan@codeclan.com>
2018-06-12 09:44:22 +0100

    removed commented out code

--- artist.py
+++ artist.py
@@ -1,8 +1,5 @@
 class Artist(object):
-    def __init__(self, name):
-        self.id = None
-        self.name = name
-
+    
     def __init__(self, name, id = None):
         self.id = id
         self.name = name
--- artist_repository.py
+++ artist_repository.py
@@ -4,37 +4,10 @@ from artist import Artist
 from sql_runner import SqlRunner
 
 class ArtistRepository(object):
-    # def __init__(self):
-    #     self.db = 'record_store'
-    #     self.table = 'artists'
-
-    # def db_connect(self):
-    #     conn = None
-    #     try:
-    #         conn=psycopg2.connect("dbname='record_store'")
-    #     except (Exception, psycopg2.DatabaseError) as error:
-    #         print(error)
-    #     return conn
-
-
+    
     def select_all(self):
         artists = []
-        # conn = None
         sql = "SELECT * FROM artists"
-        # try:
-        #     conn = self.db_connect()
-        #     cur = conn.cursor(cursor_factory=ext.DictCursor)
-        #     cur.execute(sql)
-        #     rows = cur.fetchall()
-        #     for row in rows:
-        #         artist = Artist(row['name'], row['id'])
-        #         results.append(artist)
-        #     cur.close()
-        # except (Exception, psycopg2.DatabaseError) as error:
-        #     print(error)
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
         results = SqlRunner.run(sql)
         for row in results:
             artist = Artist(row['name'], row['id'])
@@ -43,29 +16,11 @@ class ArtistRepository(object):
 
     def select(self, id):
         artist = None
-        #conn = None
         sql = "SELECT * FROM artists WHERE id = %s"
         results = SqlRunner.run(sql,(id,))
         row = results[0]
         artist = Artist(row['name'], row['id'])
         return artist
-
-        # try:
-        #     conn = self.db_connect()
-        #     cur = conn.cursor(cursor_factory=ext.DictCursor)
-        #     cur.execute(sql, (id,))
-        #     rows = cur.fetchall()
-        #     result = rows[0]
-        #     artist = Artist(result['name'], result['id'])
-        #     cur.close()
-        # except (Exception, psycopg2.DatabaseError) as error:
-        #     print(error)
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
-
-        #return artist
-
 
     def save(self, artist):
         conn = None
@@ -74,58 +29,14 @@ class ArtistRepository(object):
         id = results[0]['id']
         artist.id = id
         return artist
-        # sql = "INSERT INTO artists (name) VALUES (%s) RETURNING id"
-        # try:
-        #     conn = self.db_connect()
-        #     cur = conn.cursor()
-        #     cur.execute(sql, (artist.name,))
-        #     id = cur.fetchone()[0]
-        #     conn.commit()
-        #     cur.close()
-        #     artist.id = id
-        # except (Exception, psycopg2.DatabaseError) as error:
-        #     print(error)
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
-        # return artist
 
     def update(self, artist):
-        # updated_rows = 0
-        # sql = "UPDATE artists SET (name) = (%s) WHERE id = %s"
-        # conn = None
         updated_rows = 0
         print("ID: %d Changing to Name: %s" % (artist.id, artist.name))
         sql = "UPDATE artists SET (name) = (%s) WHERE id = %s"
         SqlRunner.run(sql,(artist.name, artist.id))
-        # try:
-        #     conn = self.db_connect()
-        #     cur = conn.cursor()
-        #     cur.execute(sql,(artist.name, artist.id))
-        #     updated_rows = cur.rowcount
-        #     conn.commit()
-        #     cur.close()
-        # except (Exception, psycopg2.DatabaseError) as error:
-        #     print(error)
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
 
     def delete(self, id):
-        #conn = None
         rows_deleted = 0
         sql = "DELETE FROM artists WHERE id = %s"
-        # try:
-        #     conn = self.db_connect()
-        #     cur = conn.cursor()
-        #     cur.execute(sql, (id,))
-        #     rows_deleted = cur.rowcount
-        #     conn.commit()
-        #     cur.close()
-        # except (Exception, psycopg2.DatabaseError) as error:
-        #     print(error)
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
-        rows_deleted = SqlRunner.run(sql,(id,)).count
-        return rows_deleted
+        SqlRunner.run(sql,(id,)).count
